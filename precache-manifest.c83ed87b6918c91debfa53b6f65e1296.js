0cd7f7a47c4fe521e5e8b15bde06040c19ad9798
Ilya Podolyak <bobandmermaid@yandex.com>
2020-08-26 15:07:55 +0300

    Updates

---
+++ precache-manifest.c83ed87b6918c91debfa53b6f65e1296.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d9bd5a1a0659d5ae161ab00d1b373f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "869b3cdf19ec2196a517",
+    "url": "/static/css/main.7c27805d.chunk.css"
+  },
+  {
+    "revision": "1345eb103767299ff9ca",
+    "url": "/static/js/2.a7f3a084.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a7f3a084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869b3cdf19ec2196a517",
+    "url": "/static/js/main.3b0d5600.chunk.js"
+  },
+  {
+    "revision": "41255e331b7701421085",
+    "url": "/static/js/runtime-main.9aeb4c57.js"
+  },
+  {
+    "revision": "5a77b029ddcd3ed0f615bc93b18e4e0b",
+    "url": "/static/media/like-active.5a77b029.svg"
+  },
+  {
+    "revision": "a70ab6c3cb6100b4da9319bcc4e1b9c8",
+    "url": "/static/media/like-inactive.a70ab6c3.svg"
+  }
+]);
